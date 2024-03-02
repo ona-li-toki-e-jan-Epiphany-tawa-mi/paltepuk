c20bd74b59c60e5ebda9c952938fe03a626059fc
ona-li-toki-e-jan-Epiphany-tawa-mi <ona-li-toki-e-jan-Epiphany-tawa-mi@protonmail.com>
2024-03-01 21:29:17 -0500

    Added descriptions for git repos.

--- modules/git.nix
+++ modules/git.nix
@@ -21,31 +21,106 @@ let gitDirectory = "/srv/git/";
 
     # A set of bare repositories to create if they don't already exist.
     repositories = [
-      "AkashicRecord.git"
-      "BASICfuck.git"
-      "BitMasher.git"
-      "COBOL-DVD-Thingy.git"
-      "Conways-Ivory-Tower.git"
-      "Explosive-Utilities.git"
-      "Explosive-Utilities.wiki.git"
-      "Glow-Chickens.git"
-      "ISawedThisPlayerInHalf.git"
-      "Incendiary-Bees.git"
-      "Modular-Backrooms.git"
-      "Nitwit-Only-Mode.git"
-      "PigsThatGoBoomInTheNight.git"
-      "PyMSWPR.git"
-      "Survival-Command-Blocks.git"
-      "VineBoomErrors-vscode.git"
-      "bungusmacs.git"
-      "cad-vault.git"
-      "cowsAyPL.git"
-      "ilo-li-sina.git"
-      "netcatchat.git"
-      "ona-li-toki-e-jan-Epiphany-tawa-mi.git"
-      "paltepuk.git"
-      "player-sounder.git"
-      "scripts-bucket.git"
+      {
+        path        = "AkashicRecord.git";
+        description = "The history of my universe, I guess";
+      }
+      {
+        path        = "BASICfuck.git";
+        description = "A brainfuck REPL for the Commodore 64";
+      }
+      {
+        path        = "BitMasher.git";
+        description = "A fast-paced text adventure game inside a ransomware-infected computer";
+      }
+      {
+        path        = "COBOL-DVD-Thingy.git";
+        description = "Terminal screensaver similar to that of DVD players";
+      }
+      {
+        path        = "Conways-Ivory-Tower.git";
+        description = "An infinitely-sized interactive implementation of Conway's Game of Life";
+      }
+      {
+        path        = "Explosive-Utilities.git";
+        description = "Nitroglycerinomancology Reborn";
+      }
+      {
+        path        = "Explosive-Utilities.wiki.git";
+        description = "Nitroglycerinomancology Reborn - Wiki";
+      }
+      {
+        path        = "Glow-Chickens.git";
+        description = "Luminescent Fetheared Bipeds";
+      }
+      {
+        path        = "ISawedThisPlayerInHalf.git";
+        description = "With the power of (brand goes here)!";
+      }
+      {
+        path        = "Incendiary-Bees.git";
+        description = "High Ordinance Apioforms";
+      }
+      {
+        path        = "Modular-Backrooms.git";
+        description = "Choose-your-experience-type Backrooms mod for Minecraft";
+      }
+      {
+        path        = "Nitwit-Only-Mode.git";
+        description = "Makes all Villagers Nitwits... because!";
+      }
+      {
+        path        = "PigsThatGoBoomInTheNight.git";
+        description = "\"Feed\" TNT to pigs to make them go BOOM!";
+      }
+      {
+        path        = "PyMSWPR.git";
+        description = "A version of Minesweeper for the CASIO fx-9750GIII (и похожие)";
+      }
+      {
+        path        = "Survival-Command-Blocks.git";
+        description = "Command Blocks now made avalible and usable in a survival world near you!";
+      }
+      {
+        path        = "VineBoomErrors-vscode.git";
+        description = "Plays the Vine boom sound effect when your badly-written code generates errors";
+      }
+      {
+        path        = "bungusmacs.git";
+        description = "Mandatory personal emacs configuration to become a LVL 100 wizard of lightning and rocks that think";
+      }
+      {
+        path        = "cad-vault.git";
+        description = "A mirror of my Thingiverse projects";
+      }
+      {
+        path        = "cowsAyPL.git";
+        description = "Cowsay in GnuAPL";
+      }
+      {
+        path        = "ilo-li-sina.git";
+        description = "\"ilo li sina\" li toki pi lawa pi ilo nanpa. taso, sina lawa ala e ona. ona li lawa e sina a!";
+      }
+      {
+        path        = "netcatchat.git";
+        description = "A simple command-line chat server and client for Linux using netcat";
+      }
+      {
+        path        = "ona-li-toki-e-jan-Epiphany-tawa-mi.git";
+        description = "poki pi nimi sona pi lipu KiApu mi";
+      }
+      {
+        path        = "paltepuk.git";
+        description = "Personal website and server wombo-combo";
+      }
+      {
+        path        = "player-sounder.git";
+        description = "Player soundser byer shellinger outer toer oneer ofer theer availableer audioer playerser";
+      }
+      {
+        path        = "scripts-bucket.git";
+        description = "A set of utility scripts and NixOS modules I have created to solve various odds and ends";
+      }
     ];
 in
 {
@@ -79,10 +154,11 @@ in
     in ''
       $DRY_RUN_CMD ${doasGit} mkdir -p ${gitDirectory}
 
-    '' + lib.concatStrings (builtins.map (repository: ''
-      if [ ! -d "${gitDirectory}/${repository}" ]; then
-        $DRY_RUN_CMD ${doasGit} mkdir -p "${gitDirectory}/${repository}"
-        $DRY_RUN_CMD ${doasGit} git -C "${gitDirectory}/${repository}" init --bare
+    '' + lib.concatStrings (builtins.map ({path, description}: ''
+      if [ ! -d "${gitDirectory}/${path}" ]; then
+        $DRY_RUN_CMD ${doasGit} mkdir -p "${gitDirectory}/${path}"
+        $DRY_RUN_CMD ${doasGit} git -C "${gitDirectory}/${path}" init --bare
+        $DRY_RUN_CMD ${doasGit} echo '${description}' >> "${gitDirectory}/${path}/description"
       fi
     '') repositories);
 
