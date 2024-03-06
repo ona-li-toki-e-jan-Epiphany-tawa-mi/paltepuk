2479f9699309b6e43c144cffefe506aa72fb8978
ona-li-toki-e-jan-Epiphany-tawa-mi <ona-li-toki-e-jan-Epiphany-tawa-mi@protonmail.com>
2024-03-06 17:24:30 -0500

    Removed unnecessary networking services.

--- hosts/raspberry-pi-3-b-plus.nix
+++ hosts/raspberry-pi-3-b-plus.nix
@@ -57,13 +57,9 @@
   # Steps up the CPU frequency.
   boot.loader.raspberryPi.firmwareConfig = [ "force_turbo=1" ];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  # networking.useDHCP = lib.mkDefault true;
-  networking.interfaces.eno0.useDHCP = lib.mkDefault true; # I only use ethernet with this server.
-  # networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true
+  # Enables networking.
+  networking.useDHCP                 = false;
+  networking.interfaces.eno0.useDHCP = true;
 
   nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
 }
