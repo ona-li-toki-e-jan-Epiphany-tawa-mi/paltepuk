d0728ab0cd178d16d137baee70e87656d87f0967
ona-li-toki-e-jan-Epiphany-tawa-mi <ona-li-toki-e-jan-Epiphany-tawa-mi@protonmail.com>
2024-03-26 20:35:57 -0400

    Added missing copyright notice on license statements.

--- modules/tor.nix
+++ modules/tor.nix
@@ -1,5 +1,7 @@
 # This file is part of paltepuk.
 #
+# Copyright (c) 2024 ona-li-toki-e-jan-Epiphany-tawa-mi
+#
 # paltepuk is free software: you can redistribute it and/or modify it under the
 # terms of the GNU Affero General Public License as published by the Free
 # Software Foundation, either version 3 of the License, or (at your option) any
