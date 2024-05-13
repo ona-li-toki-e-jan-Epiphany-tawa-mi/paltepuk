4b8bd8e79dd62021e17600a006aa573f47af47bd
ona-li-toki-e-jan-Epiphany-tawa-mi <ona-li-toki-e-jan-Epiphany-tawa-mi@protonmail.com>
2024-05-13 16:02:19 -0400

    Added automagic git mirroring service.

--- modules/git.nix
+++ modules/git.nix
@@ -49,131 +49,158 @@ let # Where to mount the git repositories directory to in the container.
         path        = "AkashicRecord.git";
         description = "The history of my universe, I guess";
         section     = sections.none;
+        autoMirror  = false;
       }
       {
         path        = "BitMasher.git";
         description = "A fast-paced text adventure game inside a ransomware-infected computer";
         section     = sections.none;
+        autoMirror  = false;
       }
       {
         path        = "COBOL-DVD-Thingy.git";
         description = "Terminal screensaver similar to that of DVD players";
         section     = sections.none;
+        autoMirror  = false;
       }
       {
         path        = "Conways-Ivory-Tower.git";
         description = "An infinitely-sized interactive implementation of Conway's Game of Life";
         section     = sections.defunct;
+        autoMirror  = false;
       }
       {
         path        = "Explosive-Utilities.git";
         description = "Nitroglycerinomancology Reborn";
         section     = sections.defunct;
+        autoMirror  = false;
       }
       {
         path        = "Explosive-Utilities.wiki.git";
         description = "Nitroglycerinomancology Reborn - Wiki | Mirror of https://github.com/ona-li-toki-e-jan-Epiphany-tawa-mi/Explosive-Utilities.wiki.git";
         section     = sections.personalMirror;
+        autoMirror  = false;
       }
       {
         path        = "Glow-Chickens.git";
         description = "Luminescent Fetheared Bipeds";
         section     = sections.defunct;
+        autoMirror  = false;
       }
       {
         path        = "ISawedThisPlayerInHalf.git";
         description = "With the power of (brand goes here)!";
         section     = sections.defunct;
+        autoMirror  = false;
       }
       {
         path        = "Incendiary-Bees.git";
         description = "High Ordinance Apioforms";
         section     = sections.defunct;
+        autoMirror  = false;
       }
       {
         path        = "Modular-Backrooms.git";
         description = "Choose-your-experience-type Backrooms mod for Minecraft";
         section     = sections.defunct;
+        autoMirror  = false;
       }
       {
         path        = "Nitwit-Only-Mode.git";
         description = "Makes all Villagers Nitwits... because!";
         section     = sections.defunct;
+        autoMirror  = false;
       }
       {
         path        = "PigsThatGoBoomInTheNight.git";
         description = "\"Feed\" TNT to pigs to make them go BOOM!";
         section     = sections.defunct;
+        autoMirror  = false;
       }
       {
         path        = "PyMSWPR.git";
         description = "A version of Minesweeper for the CASIO fx-9750GIII (и похожие)";
         section     = sections.defunct;
+        autoMirror  = false;
       }
       {
         path        = "Survival-Command-Blocks.git";
         description = "Command Blocks now made avalible and usable in a survival world near you!";
         section     = sections.defunct;
+        autoMirror  = false;
       }
       {
         path        = "VineBoomErrors-vscode.git";
         description = "Plays the Vine boom sound effect when your badly-written code generates errors";
         section     = sections.defunct;
+        autoMirror  = false;
       }
       {
         path        = "cad-vault.git";
         description = "A mirror of my Thingiverse projects";
         section     = sections.personalMirror;
+        autoMirror  = false;
       }
       {
         path        = "cowsAyPL.git";
         description = "Cowsay in GnuAPL";
         section     = sections.none;
+        autoMirror  = false;
       }
       {
         path        = "ilo-li-sina.git";
         description = "\"ilo li sina\" li toki pi lawa pi ilo nanpa. taso, sina lawa ala e ona. ona li lawa e sina a!";
         section     = sections.defunct;
+        autoMirror  = false;
       }
       {
         path        = "netcatchat.git";
         description = "A simple command-line chat server and client for Linux using netcat";
         section     = sections.none;
+        autoMirror  = false;
       }
       {
         path        = "ona-li-toki-e-jan-Epiphany-tawa-mi.git";
         description = "poki pi nimi sona pi lipu KiApu mi | Mirror of https://github.com/ona-li-toki-e-jan-Epiphany-tawa-mi/ona-li-toki-e-jan-Epiphany-tawa-mi";
         section     = sections.personalMirror;
+        autoMirror  = false;
       }
       {
         path        = "paltepuk.git";
         description = "Personal website and server wombo-combo";
         section     = sections.none;
+        autoMirror  = false;
       }
       {
         path        = "player-sounder.git";
         description = "Player soundser byer shellinger outer toer oneer ofer theer availableer audioer playerser";
         section     = sections.none;
+        autoMirror  = false;
       }
       {
         path        = "epitaphpkgs.git";
         description = "My Nix User Repository | Mirror of https://github.com/ona-li-toki-e-jan-Epiphany-tawa-mi/epitaphpkgs";
         section     = sections.personalMirror;
+        autoMirror  = false;
       }
       {
         path        = "Brainblast-Toolkit.git";
         description = "A brainfuck/BASICfuck REPL for 6502 machines";
         section     = sections.none;
+        autoMirror  = false;
       }
       {
         path        = "nixpkgs.git";
-        description = "Nix Packages collection & NixOS";
+        description = "Nix Packages collection & NixOS | Mirror of https://github.com/nixos/nixpkgs";
         section     = sections.mirror;
+        autoMirror  = true;
+        mirrorUrl   = "https://github.com/nixos/nixpkgs";
       }
       {
         path        = "hydra-projects.git";
         description = "Declarative Hydra project specifications";
         section     = sections.none;
+        autoMirror  = false;
       }
     ];
 in
@@ -195,6 +222,9 @@ in
   system.activationScripts."create-git-bind-mounts" = ''
     mkdir -p ${directories.gitRepositories} ${directories.gitSSH}
   '';
+
+  # Gives the container internet access.
+  networking.nat.internalInterfaces = [ "ve-${serviceNames.git}" ];
 
   # Isolated container for the git server and cgit to run in.
   containers."${serviceNames.git}" = (import ./lib/default-container.nix {inherit vlan; inherit vlan6;}) // {
@@ -251,25 +281,63 @@ in
 
       services.openssh.settings."AllowUsers" = [ serviceNames.git ];
 
-      # Creates any specified repositories if they don't already exist.
-      systemd.services."create-repositories" = {
-        description = "git repository creation service";
-        wantedBy    = [ "multi-user.target" ];
-        path        = [ pkgs.git ];
+      systemd = {
+        services = {
+          # Creates any specified repositories if they don't already exist.
+          "create-repositories" = {
+            description = "git repository creation service";
+            wantedBy    = [ "multi-user.target" ];
+            path        = [ pkgs.git ];
 
-        script = lib.concatStrings (builtins.map ({path, ...}: ''
-            if [ ! -d "${path}" ]; then
-              mkdir -p "${path}"
-              git -C "${path}" init --bare
-            fi
-          '')
-          repositories
-        );
+            script = lib.concatStrings (builtins.map ({ path, ... }: ''
+              if [ ! -d "${path}" ]; then
+                mkdir -p "${path}"
+                git -C "${path}" init --bare
+              fi
+            '')
+              (builtins.filter ({ autoMirror, ... }: !autoMirror) repositories)
+            );
 
-        serviceConfig = {
-          "Type"             = "oneshot";
-          "User"             = serviceNames.git;
-          "WorkingDirectory" = gitDirectory;
+            serviceConfig = {
+              "Type"             = "oneshot";
+              "User"             = serviceNames.git;
+              "WorkingDirectory" = gitDirectory;
+            };
+          };
+
+          # Automatically mirrors and updates mirror repositories.
+          "auto-mirror" = {
+            description = "git repository automirroring service";
+            wantedBy    = [ "multi-user.target" ];
+            path        = [ pkgs.git ];
+
+            script = lib.concatStrings (builtins.map ({ path, mirrorUrl, ... }: ''
+              if [ ! -d "${path}" ]; then
+                git clone --mirror "${mirrorUrl}" "${path}"
+              else
+                git -C "${path}" remote update
+              fi
+            '')
+              (builtins.filter ({ autoMirror, ... }: autoMirror) repositories)
+            );
+
+            serviceConfig = {
+              "Type"             = "oneshot";
+              "User"             = serviceNames.git;
+              "WorkingDirectory" = gitDirectory;
+            };
+          };
+        };
+
+        # Runs the auto-mirror service once a week.
+        timers."auto-mirror" = {
+          description = "git repository automirroring service";
+          wantedBy    = [ "timers.target" ];
+
+          timerConfig = {
+            "OnCalendar" = "weekly";
+            "Persistent" = true;
+          };
         };
       };
 
