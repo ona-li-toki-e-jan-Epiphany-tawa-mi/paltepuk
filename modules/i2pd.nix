5634c4e1ab792be27441e86286b52d14b772867f
ona-li-toki-e-jan-Epiphany-tawa-mi <ona-li-toki-e-jan-Epiphany-tawa-mi@protonmail.com>
2025-02-03 15:01:29 -0500

    Extracted out i2pd to allow changing tunnel types
    Also added systemd security settings

--- modules/i2pd.nix
+++ modules/i2pd.nix
@@ -24,12 +24,15 @@
 , pkgs
 , extraPorts
 , auth
+, config
 , ...
 }:
 
 let inherit (lib) mkForce;
     inherit (builtins) toString;
-    inherit (pkgs) writeShellApplication;
+    inherit (pkgs) writeShellApplication writeText;
+
+    i2pdDirectory = "/var/lib/i2pd";
 in
 {
   # Allows conenctions from peers
@@ -49,29 +52,109 @@ in
     '';
   }) ];
 
-  services.i2pd = {
-    package    = pkgs-unstable.i2pd;
-    enable     = true;
-    enableIPv6 = true;                            # Based.
-    port       = extraPorts.i2pd;
 
-    # Normally this has a couple eepsites to pull domain names from, but we're
-    # just using I2P for running "hidden" services.
-    addressbook.subscriptions = mkForce [];
 
-    # Web console.
-    proto.http = with auth.i2pd; {
-      enable = true;
-      port   = ports.i2pdConsole;
-      auth   = true;
-      user   = username;
-      pass   = password;
-    };
+  # We do all this stuff instead of using services.i2pd because it doesn't
+  # support setting the type of tunnel, only server for in-tunnels and client
+  # for out-tunnels.
 
-    inTunnels."nginx" = {
-      enable      = true;
-      port        = 80;
-      destination = "";
-    };
+  users.users.i2pd = {
+    group       = "i2pd";
+    description = "I2Pd User";
+    home        = i2pdDirectory;
+    createHome  = true;
+    uid         = config.ids.uids.i2pd;
   };
+
+  users.groups.i2pd.gid = config.ids.gids.i2pd;
+
+  systemd.services.i2pd =
+    let i2pdTunnelsConf = writeText "i2pd-tunnels.conf" ''
+          [nginx]
+          type = http
+          host = 127.0.0.1
+          port = 80
+          keys = nginx-keys.dat
+        '';
+
+        i2pdConf = writeText "i2pd.conf" ''
+          ipv4      = true
+          ipv6      = true # Based.
+          bandwidth = ${toString config.services.i2pd.bandwidth}
+          port      = ${toString extraPorts.i2pd}
+
+          # Web console.
+          [http]
+          address = 127.0.0.1
+          port    = ${toString ports.i2pdConsole}
+          auth    = true
+          user    = ${auth.i2pd.username}
+          pass    = ${auth.i2pd.password}
+
+          # Disables HTTP proxy.
+          [httpproxy]
+          enabled = false
+          # Disables SOCKS proxy.
+          [socksproxy]
+          enabled = false
+          # Disables SAM interface.
+          [sam]
+          enabled = false
+          # Disables I2CP interface.
+          [i2cp]
+          enabled = false
+
+          # Enables more caching.
+          [precomputation]
+          elgamal = true
+
+          # Disables addressbook since we just want to run services.
+          [addressbook]
+          enabled = false
+        '';
+    in {
+      description = "Minimal I2P router";
+      after       = [ "network.target" ];
+      wantedBy    = [ "multi-user.target" ];
+      path        = [ pkgs-unstable.i2pd ];
+
+      script = ''
+        i2pd --service --conf=${i2pdConf} --tunconf=${i2pdTunnelsConf}
+      '';
+
+      serviceConfig = {
+        User             = "i2pd";
+        WorkingDirectory = i2pdDirectory;
+        Restart          = "on-abort";
+
+        # systemd-analyze security recommendations.
+        PrivateDevices          = true;
+        ProtectClock            = true;
+        ProtectKernelLogs       = true;
+        RemoveIPC               = true;
+        NoNewPrivileges         = true;
+        ProtectControlGroups    = true;
+        ProtectKernelModules    = true;
+        MemoryDenyWriteExecute  = true;
+        SystemCallArchitectures = [ "native" ];
+        ProtectHostname         = true;
+        ProtectSystem           = "strict";
+        ReadWritePaths          = [ i2pdDirectory ];
+        ProtectProc             = "invisible";
+        LockPersonality         = true;
+        RestrictRealtime        = true;
+        ProcSubset              = "pid";
+        ProtectHome             = true;
+        PrivateUsers            = true;
+        PrivateTmp              = true;
+        SystemCallFilter        =
+          [ "@system-service" "~@resources" "~@privileged" ];
+        SystemCallErrorNumber   = "EPERM";
+        RestrictAddressFamilies = [ "AF_INET" "AF_INET6" ];
+        ProtectKernelTunables   = true;
+        RestrictNamespaces      = true;
+        RestrictSUIDSGID        = true;
+        CapabilityBoundingSet   = "";
+      };
+    };
 }
