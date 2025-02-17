b55350d7ef9a36038c1466208d8201e213aea465
ona-li-toki-e-jan-Epiphany-tawa-mi <ona-li-toki-e-jan-Epiphany-tawa-mi@protonmail.com>
2025-02-17 15:14:34 -0500

    Removed Luanti port from flake.nix

--- flake.nix
+++ flake.nix
@@ -44,7 +44,6 @@
           ports = {
             i2pdConsole = 7070;
             cgit        = 5000;
-            luanti      = 30000;
           };
         } // (import ./config.nix);
     in {
